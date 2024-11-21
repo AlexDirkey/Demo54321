15359056b9400c11539f89cff1c5008408b1e2ed
AlexDirkey <91528396+AlexDirkey@users.noreply.github.com>
2024-11-21 09:47:36 +0100

    Kæmpe justeringer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world, dette er en stor fed test");
     }
+
+
 }
